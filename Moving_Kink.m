bdaa3b2ce70ca1879194e239736638e66b9948e8
ForeignOx <164361658+ForeignOx@users.noreply.github.com>
2025-12-19 10:53:00 +0000

    Update Moving_Kink.m

--- Moving_Kink.m
+++ Moving_Kink.m
@@ -12,17 +12,15 @@ v=0.5;
 gamma = 1/sqrt((1-v^2));
 
 % Initial condition
-p0 = tanh(gamma*(2*x-a)) ;
-
-
+p0 = tanh(gamma*(x-a)) ;
 
 
 % Initialize the spatial parameters for simulation
-T = 20; dt = 0.01; time = 0:dt:T;
+T = 15; dt = 0.01; time = 0:dt:T;
 
 % Preallocate the solution array
 phi = zeros(N, length(time));
-p1 = tanh(gamma*(2*x-a-v*dt));
+p1 = tanh(gamma*(x-a-v*dt));
 
 
 phi(:, 1) = p0;
@@ -33,7 +31,6 @@ phi(:, 2) = p1; % Set initial condition
 one_vec = ones(N,1);
 Laplacian = spdiags([one_vec, -2*one_vec, one_vec],[1,0,-1],N,N);
 % Set the boundary conditions
-% Laplacian(1,1) = -2; Laplacian(N,N) = -2;
 Laplacian = (1/dx^2)*Laplacian;
 
 e = zeros(N,1);
@@ -47,22 +44,46 @@ end
 
 
 close all;
-plot(phi(:,1))
 
 
 figure;
-saveToGif(x,phi,"animated.gif",[-L,L,-2,2],100)
+vid = VideoWriter('moving_kink_video.mp4', 'MPEG-4');
+vid.FrameRate = 30;
+open(vid)
+saveToGif(x,phi,vid,[-L,L,-2,2],150,v,gamma,T)
 
 
-function saveToGif(x,phi,name,axs,frames)
-    plt = plot(x,phi(:,1));
+figure;
+plot(x,phi(:,1),LineWidth=2)
+hold on;
+plot(x,phi(:,round(end/2)),LineWidth=2)
+plot(x,phi(:,end),LineWidth=2)
+plot(x,tanh(gamma*x),'--')
+plot(x,tanh(gamma*(x-v*T/2*ones(size(x)))),'--')
+plot(x,tanh(gamma*(x-v*T*ones(size(x)))),'--')
+legend('$t=0$','$t=T/2$','$t=T$','$\tanh(\gamma x)$','$\tanh(\gamma (x-vT/2))$','$\tanh(\gamma (x-vT))$','Interpreter','latex','Location','best','FontSize',20)
+xlabel('$x$','Interpreter','latex','FontSize',24)
+ylabel('$\phi$','Interpreter','latex','FontSize',24)
+
+function saveToGif(x,phi,vid,axs,frames,v,gamma,T)
+    plt1 = plot(x,phi(:,1),'LineWidth',2);
+    hold on;
+    plt2 = plot(x,tanh(gamma*x),'c--');
     axis(axs)
+    xlabel("$x$",Interpreter='latex',FontSize=14)
+    ylabel("$\phi$",Interpreter='latex',FontSize=14)
+    title('Animation of Moving Kink','FontSize',20)
+    legend('Numerical Approximation','Exact Solution',FontSize=14)
     for i = 1:frames
+        frame= getframe(gcf);
         %exportgraphics(gcf,name,'Append',true);
+        writeVideo(vid, frame.cdata);
         axis(axs)
-        plt.YData = phi(:,round(i*end/frames)); % Update plot data for the current frame
+        plt1.YData = phi(:,round(i*end/frames)); % Update plot data for the current frame
+        plt2.YData = tanh(gamma*(x-v*round(i)*T/frames*ones(size(x)))); % Update plot data for the current frame
         drawnow; % Update the figure window
         %pause(0.1); % Pause to control the frame rate
     end
 end
 
+close(vid);
